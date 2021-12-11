5c3deacb3a1fd00d9f69815ad8cc638590101714
leonaysmael16 <ysmealleona@gmail.com>
2021-12-11 00:11:12 -0800

    mightve broke things

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,9 +4,16 @@ export default function aboutMe() {
     return (
         <section id="about me">
            <div>
-               <h2 class="center-align">Leona Ysmael React Portfolio</h2>
+               <h2 class="text-white center-align">Leona Ysmael React Portfolio</h2>
             </div> 
-            
+            <div>
+                <p class="text white center-align">
+                    Hello There! My name is Leona Ysmael and I am Web Developer with a diverse background of 
+                    experiences such as Selling, being a Barista, and at home learning Web Developer. I am current 
+                    student of the University of Washington Coding Bootcamp to expand my knowledge and skills about the 
+                    Web Development world. Come check out my projects I have made myself and with my fellow classmates.
+                    </p>
+            </div>
         </section>
     )
 } 

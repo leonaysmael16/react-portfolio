102aabd5ed37a4ad7bbdc666dbc92ad6b884975a
leonaysmael16 <ysmealleona@gmail.com>
2021-12-10 23:43:23 -0800

    added files and updated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./components/AboutMe";
+import Project from "./components/Projects";
+import Contact from "./components/ContactMe";
 
 
 function App() {
   return (
-    <div>
+    <div class="pink accent-1">
       <Navbar />
       <About />
       <Project />
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,2 +1,13 @@
 import React from "react";
 
+export default function aboutMe() {
+    return (
+        <section id="about me">
+           <div>
+               <h2 class="center-align">Leona Ysmael React Portfolio</h2>
+            </div> 
+            
+        </section>
+    )
+} 
+
